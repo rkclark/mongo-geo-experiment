cb28f05de131cecd4f11eb3388b827ba0a256cfc
Rick Clark <rkingsleyclark@gmail.com>
2018-02-19 13:51:04 +0000

    Add geo test

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-if(process.argv[2] === undefined) {
-  console.error('Usage: `npm start SEARCH_QUERY`')
+if (process.argv[2] === undefined) {
+  console.error("Usage: `npm start SEARCH_QUERY`");
   process.exit();
 }
 
-const MongoClient = require('mongodb').MongoClient;
-const restaurantData = require('./restaurants.json')
-const url = 'mongodb://localhost:27017';
- 
+console.log("Args: ", process.argv[2], process.argv[3]);
+
+let queryStart;
+let queryEnd;
+
+const MongoClient = require("mongodb").MongoClient;
+const restaurantData = require("./restaurants.json");
+const url = "mongodb://localhost:27017";
+
 // Database Name
-const dbName = 'Restaurant-spike';
+const dbName = "Restaurant-spike";
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
-  
   const db = client.db(dbName);
   indexCollection(db, () => {
     console.log("index created");
     insertData(db, () => {
       console.log("data inserted");
-
-      queryRestaurants(db, () => {
-
-      });
+      queryStart = new Date();
+      queryRestaurants(db, () => {});
       client.close();
     });
   });
@@ -28,17 +30,18 @@ MongoClient.connect(url, function(err, client) {
 
 /**
  * Create fulltext index on all fields containing text and geo index.
- * @param {*} db 
- * @param {*} callback 
+ * @param {*} db
+ * @param {*} callback
  * https://docs.mongodb.com/manual/tutorial/control-results-of-text-search/
  */
 const indexCollection = (db, callback) => {
   // https://docs.mongodb.com/manual/reference/command/createIndexes/
-  db.collection('restaurants').createIndexes(
+  db.collection("restaurants").createIndexes(
     [
-      { "key": { "geo": "2dsphere" } },
-      { "key": { 
-          "name": "text",
+      { key: { geo: "2dsphere" } },
+      {
+        key: {
+          name: "text",
           "address.building": "text",
           "address.town": "text",
           "address.street": "text",
@@ -47,15 +50,15 @@ const indexCollection = (db, callback) => {
           "address.country": "text"
         },
         // Everything else defaults to 1
-        "weights": {
-          "name": 10,
+        weights: {
+          name: 10,
           "address.town": 5,
           "address.county": 4
-        }, 
-        "default_language": "english",
-        "name": "TextIndex"
+        },
+        default_language: "english",
+        name: "TextIndex"
       },
-      { "key": { "id": 1}, "unique": true}
+      { key: { id: 1 }, unique: true }
     ],
     {},
     (err, result) => {
@@ -65,38 +68,75 @@ const indexCollection = (db, callback) => {
   );
 };
 
-const insertData = (db, callback)  => {
+const insertData = (db, callback) => {
   // console.log(restaurantData);
   restaurantData.forEach(element => {
-    db.collection('restaurants').replaceOne(
-      {id: element.id},
-      element,
-      {upsert: true}
-    )
+    db
+      .collection("restaurants")
+      .replaceOne({ id: element.id }, element, { upsert: true });
   });
   callback();
-}
-
-const queryRestaurants = (db, callback) => {
-  const cursor = 
-    db.collection('restaurants')
-      .find( 
-        {
-          '$text': {
-            '$search': process.argv[2] 
-          },
-          "geo": { $geoWithin: { $center: [ [-0.043945, 51.482452], 1 ] } } 
-        },
-        { 
-          'projection': {'score': { $meta: "textScore" } }
-        } 
-      )
-      .sort({ score: { $meta: "textScore" } })
-      .limit(10);
-
-  cursor.toArray((err, docs) => {
-    console.log(err, docs);
-    callback(docs);
-  });
 };
 
+// const queryRestaurants = (db, callback) => {
+//   const cursor =
+//     db.collection('restaurants')
+//       .find(
+//         {
+//           '$text': {
+//             '$search': process.argv[2]
+//           },
+//           "geo": { $geoWithin: { $center: [ [-0.043945, 51.482452], 1 ] } }
+//         },
+//         {
+//           'projection': {'score': { $meta: "textScore" } }
+//         }
+//       )
+//       .sort({ score: { $meta: "textScore" } })
+//       .limit(10);
+
+//   cursor.toArray((err, docs) => {
+//     console.log(err, docs);
+//     callback(docs);
+//   });
+// };
+
+// const queryRestaurants = (db, callback) => {
+//   const cursor = db
+//     .collection("restaurants")
+//     .find({
+//       geo: {
+//         $near : {
+//           $geometry : {
+//              coordinates : [parseFloat(process.argv[2]), parseFloat(process.argv[3])]
+//             },
+//          $maxDistance : 100000
+//         }
+//       }
+//     })
+//     .limit(100);
+
+//   cursor.toArray((err, docs) => {
+//     console.log(err, docs);
+//     console.log(`FOUND ${docs.length} RESULTS`);
+//     queryEnd = new Date() - queryStart;
+//     console.log("TIME TAKEN: ", queryEnd);
+//     callback(docs);
+//   });
+// };
+
+const queryRestaurants = async (db, callback) => {
+  const cursor = await db.command({
+    geoNear: "restaurants",
+    near: {
+      type: "Point",
+      coordinates: [parseFloat(process.argv[2]), parseFloat(process.argv[3])]
+    },
+    spherical: true,
+    maxDistance: 100000
+  });
+  console.log(cursor);
+  console.log(`FOUND ${cursor.results.length} RESULTS`);
+  queryEnd = new Date() - queryStart;
+  console.log("TIME TAKEN: ", queryEnd);
+};
